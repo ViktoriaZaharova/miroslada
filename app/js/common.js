9353dc8af20f6a4497986fadbf560eb7708b4cb8
Viktoria <zaharova181114@gmail.com>
2022-02-03 13:47:54 +0200

    add pages

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,5 @@
 $('.btn-toggle').on('click', function () {
-   $('.mobile-menu').fadeToggle();
+    $('.mobile-menu').fadeToggle();
 });
 
 $('.btn-close').on('click', function () {
@@ -7,5 +7,37 @@ $('.btn-close').on('click', function () {
 });
 
 $('[name="phone"]').mask('+7 (999) 999-99-99');
+
+// активная ссылка меню
+$('.menu li a').each(function () {
+    let location = window.location.href;
+    let link = this.href;
+    if (location === link) {
+        $(this).addClass('active');
+    }
+});
+// end
+
+$('.catalog-slider').slick({
+    slidesToShow: 1,
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/prev.png" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/prev.png" alt=""></button>',
+});
+
+$('.product-gallery-max').slick({
+    slidesToShow: 1,
+    fade: true,
+    arrows: false,
+    asNavFor: '.product-gallery-preview'
+});
+
+$('.product-gallery-preview').slick({
+    slidesToShow: 3,
+    appendArrows: '.slider-nav',
+    asNavFor: '.product-gallery-max',
+    focusOnSelect: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/prev.png" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/prev.png" alt=""></button>',
+});
 
 new WOW().init();
---
+++ app/js/map.js
@@ -0,0 +1,43 @@
+// Инициализация карты
+ymaps.ready(init);
+
+function init () {
+
+    //Центрирование и выбор масштаба карты
+    var myMap = new ymaps.Map('map', {
+        center: [53.127208, 45.020804],
+        zoom: 16
+    });
+
+    // Создание своей метки
+    var myPlacemark = new ymaps.Placemark(
+        // Координаты метки
+        [53.127208, 45.020804] , {
+            // Свойства метки
+            hintContent: '', //Подсказка при наведении на маркер
+            iconContent: '<div class="loc-name">ООО «Кондитерская\n' +
+                'Фабрика Мирослада»</div>',
+
+        }, {
+            iconImageHref: 'img/marker.png',  // картинка иконки
+            iconImageSize: [85, 120],                                      // размеры картинки
+            iconImageOffset: [-70, -40],// смещение картинки
+
+        });
+
+    // Добавление метки на карту
+    myMap.geoObjects.add(myPlacemark);
+
+    //Элементы управления
+    myMap.controls
+    // Кнопка изменения масштаба
+        .add('zoomControl')
+        // Список типов карты
+        .add('typeSelector')
+        // Кнопка изменения масштаба - справа
+        .add('smallZoomControl', { right: 5, top: 75 })
+        // Стандартный набор кнопок
+        .add('mapTools')
+        //Линейка масштаба
+        .add(new ymaps.control.ScaleLine());
+}
